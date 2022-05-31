dbb3244482c54a6570debe369867f4705e638f8c
Yusril Zubaydi <yusril.19051@mhs.its.ac.id>
2022-05-31 15:35:16 +0700

    Add Main Menu

---
+++ FP_GC_2D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play(){
+        SceneManager.LoadScene("Level 1");
+    }
+    public void LevelSelect(){
+        SceneManager.LoadScene("LevelSelector");
+    }
+    public void Quit(){
+        Application.Quit();
+        Debug.Log("quit");
+    }
+}
